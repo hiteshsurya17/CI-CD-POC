70b2e38d312139599defbe6516513649a6e5da29
hiteshsurya17 <83537863+hiteshsurya17@users.noreply.github.com>
2025-07-03 04:29:41 -0500

    Create vault.tf

---
+++ TerraformFiles/vault.tf
@@ -0,0 +1,16 @@
+provider "vault" {
+  address = "https://vault.example.com"
+  token   = var.vault_token
+}
+
+resource "vault_kv_secret_v2" "flask_app_secrets" {
+  mount = "secret"
+  name  = "flask-app"
+
+  data_json = jsonencode({
+    db_password     = "supersecret-db-pass"
+    api_key         = "abcd1234-api-key"
+    aws_access_key  = "AKIAEXAMPLE123456789"
+    aws_secret_key  = "supersecretawskey987654321"
+  })
+}
